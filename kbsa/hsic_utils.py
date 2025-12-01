e6f48287b0de95eab88dca898841955f6b00999c
Farbod Chamanian <farbod.ch.96@gmail.com>
2025-12-01 14:36:20 +0100

    more prototyping

--- kbsa/hsic_utils.py
+++ kbsa/hsic_utils.py
@@ -2,10 +2,20 @@ from pathlib import Path
 import ast
 from dolfin import Mesh, FunctionSpace, Function, XDMFFile
 from numpy import ndarray, float32, uint8, fill_diagonal, array as np_array
+from numpy import mean as np_mean
 from numpy import zeros as np_zeros
+from numpy import load as np_load
 from numpy import arange as np_arange
 from numpy.random import shuffle as np_shuffle
 from numpy.random import uniform as np_unif
+from numpy import abs as np_abs
+from numpy import prod as np_prod
+from numpy import where as np_where
+from numpy import exp as np_exp
+from numpy import sum as np_sum
+from numpy import triu_indices as np_triu_indices
+from time import time_ns
+from utils.other_utils import flipStr, getIndexSuperset, directBinStrSum
 import gc
 
 def parse_meta_data(meta_file: Path):
@@ -29,17 +39,24 @@ def parse_meta_data(meta_file: Path):
         print(f"Could not parse cdr_params in {meta_file}")
         return None
 
-def get_field_file_dirs(base_dir: str, chosen_cdr_field: str):
+def get_data_file_dirs(base_dir: str, data_type: str):
     """
-    chosen_cdr_field ∈ {'fuel_field', 'oxygen_field', 'product_field', 'temp_field'}
+    data_type ∈ {'fuel_field', 'oxygen_field', 'product_field', 'temp_field', 'input_data'}
     Returns a list of file paths for chosen_cdr_field across all qualified sub_sub_directories.
     """
     base_dir = Path(base_dir)   
 
-    if chosen_cdr_field not in ['fuel_field', 'oxygen_field', 'product_field', 'temp_field']:
-        raise ValueError("chosen cdr field must be one of 'fuel_field', 'oxygen_field', 'product_field', 'temp_field'")
+    if data_type not in ['fuel_field', 'oxygen_field', 'product_field', 'temp_field', 'input_data']:
+        raise ValueError("chosen cdr field must be one of 'fuel_field', 'oxygen_field', 'product_field', 'temp_field', 'input_data'")
+    
 
-    target_filename = f"{chosen_cdr_field}.h5"
+    target_filename = data_type
+    if data_type in ['fuel_field', 'oxygen_field', 'product_field', 'temp_field']:
+        target_filename += '.h5'
+    else:
+        target_filename += '.npy'
+    # target_filename = f"{chosen_cdr_field}.h5"
+
     collected_paths = []
 
     num_of_parent_skips = 0
@@ -90,7 +107,7 @@ def get_field_file_dirs(base_dir: str, chosen_cdr_field: str):
                 # print(f"Skipping {parent}/{sub}: does not have 6 sub_sub-folders.")
                 continue
 
-            #3)check inside each sub_sub_directory for exactly 9 files
+            #3)check inside each sub_sub_directory for exactly 10 files
             for sub_sub in sub_subs:
                 files = [f for f in sub_sub.iterdir() if f.is_file()]
                 if len(files) != 10:
@@ -98,10 +115,13 @@ def get_field_file_dirs(base_dir: str, chosen_cdr_field: str):
                     # print(f"Skipping {parent}/{sub}/{sub_sub}: does not contain 9 files.")
                     continue
 
-                #4)collect chosen H5 file
+                #4)collect chosen .h5 or .npy file
                 wanted_path = sub_sub / target_filename
                 if wanted_path.exists():
-                    collected_paths.append(str(wanted_path)[:-3])
+                    if data_type == 'input_data':
+                        collected_paths.append(str(wanted_path))
+                    else:
+                        collected_paths.append(str(wanted_path)[:-3])
                 else:
                     num_of_sub_sub_folder_skips += 1
                     print(f"Warning: {wanted_path} missing.")
@@ -113,7 +133,6 @@ def get_field_file_dirs(base_dir: str, chosen_cdr_field: str):
         #     continue
 
     return collected_paths
-
 
 def load_mesh(mesh_dir: str = "data/CDR/mesh_save_dir/rectangle.xdmf"):
     mesh = Mesh()
@@ -173,64 +192,294 @@ def sample_fenics_function(field_file_path: str,
     else:
         return f_samplings
 
-def approximate_k_set(lambda_matrix: ndarray,
-                    lambda_X: float) -> ndarray:
-    _, m = lambda_matrix.shape
+def approximate_set_lebesgue(binary_system_output_data: ndarray,
+                            lambda_X: float) -> ndarray:
+    
+    _, m = binary_system_output_data.shape
 
     #row sums /w output shape (n,)
-    just_ones_count = lambda_matrix.sum(axis=1)
+    just_ones_count = binary_system_output_data.sum(axis=1)
 
     #pairwise AND count /w output shape (n,n)
-    and_count = lambda_matrix @ lambda_matrix.T
+    and_count = binary_system_output_data @ binary_system_output_data.T
 
     #xOr count using identity AxOrB=a+b-2(aANDb)
     xOr_count = just_ones_count[:, None] + just_ones_count[None, :] - 2*and_count
     xOr_count = xOr_count.astype(float32)
 
-    gamma_matrix = lambda_X*(xOr_count/m)
-    fill_diagonal(gamma_matrix, 0.0)
+    lambda_matrix = lambda_X*(xOr_count/m)
+    
+    return lambda_matrix
 
-    return gamma_matrix
-
-
-def get_gamma_matrix(data_dir_list_of_fields: list, 
-                    n: int, 
-                    num_of_spatial_sampling_m: int,
-                    mesh: Mesh,
-                    func_space_V: FunctionSpace,
-                    test_domain: ndarray = np_array([[0,1],[0,0.5]]),
-                    return_as_bool: bool = False,
-                    g_constraint: float = 0):
+def get_K_gamma(field_data_dirs_list: list,
+                n: int,
+                num_of_spatial_sampling_m: int,
+                mesh: Mesh,
+                func_space_V: FunctionSpace,
+                test_domain: ndarray = np_array([[0,1],[0,0.5]]),
+                return_as_bool: bool = False,
+                g_constraint: float = 0,
+                verbose: bool = False):
     
     m = num_of_spatial_sampling_m
-    n_max = len(data_dir_list_of_fields)
-    data_dir_indices = np_arange(0, n_max)
-    np_shuffle(data_dir_indices)
-    if n < n_max:
-        data_dir_indices = data_dir_indices[:n]
-    else:
-        n = n_max
+    binary_system_output_data = np_zeros((n, m), dtype=uint8)
 
-    lambda_matrix = np_zeros((n, m), dtype=uint8)
-    for i, data_dir_idx in enumerate(data_dir_indices):
-        lambda_matrix[i] = sample_fenics_function(field_file_path=data_dir_list_of_fields[data_dir_idx],
+    if verbose:
+        t0_0 = time_ns()
+    for i, dir in enumerate(field_data_dirs_list):
+        binary_system_output_data[i] = sample_fenics_function(field_file_path=dir,
                                                 mesh=mesh,
                                                 func_space_V=func_space_V,
                                                 test_domain=test_domain,
                                                 num_of_spatial_sampling_m=m,
                                                 return_as_bool=return_as_bool,
                                                 g_constraint=g_constraint)
-    
+    if verbose:
+        t0_1 = time_ns()
     lambda_X = 1
     for i in range(len(test_domain)):
         lambda_curr_dom = max(test_domain[i])-min(test_domain[i])
         if lambda_curr_dom != 0:
             lambda_X *= lambda_curr_dom
+    if verbose:
+        t1_0 = time_ns()
+
+    lambda_matrix = approximate_set_lebesgue(binary_system_output_data=binary_system_output_data, lambda_X=lambda_X)
+
+    if verbose:
+        t1_1 = time_ns()
+
+    sigma_squared = np_mean(lambda_matrix)
+
+    if verbose:
+        t2_1 = time_ns()
+
+    K_gamma = np_exp(-1.0*lambda_matrix/(2*sigma_squared))
+
+    if verbose:
+        t3_1 = time_ns()
+
+    fill_diagonal(K_gamma, 0.0)
+
+    if verbose:
+        t4_1 = time_ns()
+        dt_spatial_sampling = (t0_1-t0_0) / 1e9
+        dt_approximate_set_lebesgue = (t1_1-t1_0) / 1e9
+        dt_sigma_squared = (t2_1-t1_1) / 1e9
+        dt_exponentiate_K_gamma = (t3_1-t2_1) / 1e9
+        dt_fill_diag_of_K_gamma = (t4_1-t3_1) / 1e9
+        print(f'Wall-clock time of spatial_sampling: {dt_spatial_sampling:.3f} (s)')
+        print(f'Wall-clock time of approximate_set_lebesgue: {dt_approximate_set_lebesgue:.3f} (s)')
+        print(f'Wall-clock time of sigma_squared: {dt_sigma_squared:.3f} (s)')
+        print(f'Wall-clock time of exponentiate_K_gamma: {dt_exponentiate_K_gamma:.3f} (s)')
+        print(f'Wall-clock time of fill_diag_of_K_gamma: {dt_fill_diag_of_K_gamma:.3f} (s)')
+
+    return K_gamma
+
+def sobolev_kernel_univar(u_i, u_j):
+    du = u_i-u_j
+    return (1.0 + (u_i-0.5)*(u_j-0.5) + 0.5*(du*du-np_abs(du)+1.0/6.0))
+
+def get_K_U_sobolev_looped(data_directory: str = None,
+                    n: int = 5,
+                    num_of_inputs: int = 5,
+                    which_input_one_hot: str = '00001',
+                    input_data=None,
+                    verbose: bool = False):
+    if verbose:
+        t0 = time_ns()    
+
+    dim_U = num_of_inputs
+    if input_data is None:
+        input_data_dirs_list = get_data_file_dirs(data_directory, data_type='input_data')
+        u_arr = np_zeros((n, dim_U))
+        for i, dir in enumerate(input_data_dirs_list):
+            u_arr[i] = np_load(dir)
+    else:
+        u_arr = input_data
+
+    which_input_mask = np_array([c == '1' for c in which_input_one_hot], dtype=bool)
+    active_dims = np_where(which_input_mask)[0]
+
+    if len(active_dims) == 0:
+        raise ValueError("which_input_one_hot must include at least one dimension.")
+
+    K_sob_matrix = np_zeros((n,n), dtype=float)
+
+    for j in range(n):
+        for i in range(j+1):
+            K_ij = 1.0
+
+            for d in active_dims:
+                u_i = u_arr[i, d]
+                u_j = u_arr[j, d]
+                K_ij *= sobolev_kernel_univar(u_i, u_j)
+            K_sob_matrix[i,j] = K_ij
+            K_sob_matrix[j,i] = K_ij
+    if verbose:
+        t1 = time_ns()
+        dt = (t1-t0) / 1e9
+        print(f'Wall-clock time of get_K_U_sobolev_looped: {dt:.3f} (s)')
+    return K_sob_matrix
+
+def get_K_U_sobolev_vectorized(data_directory: str = None,
+                    n: int = 5,
+                    num_of_inputs: int = 5,
+                    which_input_one_hot: str = '00001',
+                    chunk_size=None,
+                    input_data=None,
+                    verbose:bool=False):
+    if verbose:
+        t0 = time_ns()
+    dim_U = num_of_inputs
+    if input_data is None:
+        input_data_dirs_list = get_data_file_dirs(data_directory, data_type='input_data')
+        u_arr = np_zeros((n, dim_U))
+        for i, dir in enumerate(input_data_dirs_list):
+            u_arr[i] = np_load(dir)
+    else:
+        u_arr = input_data
+
+    which_input_mask = np_array([c == '1' for c in which_input_one_hot], dtype=bool)
+    u_active = u_arr[:, which_input_mask]
+    if chunk_size is None or chunk_size <= 0:
+        chunk_size = n
+
+    K_sob_matrix = np_zeros((n,n), dtype=float)
+
+    start_idx = 0
+    while start_idx < n:
+        end_idx = min(start_idx + chunk_size, n)
+
+        U_i = u_active[start_idx:end_idx]
+        u_i = U_i[:, :, None]
+        u_j = u_active.T[None, :, :]
+        du = u_i - u_j
+        K_curr_chunk_sobolev = (1.0 + (u_i-0.5)*(u_j-0.5) + 0.5*(du*du-np_abs(du)+1.0/6.0))
+        K_sob_matrix[start_idx:end_idx] = np_prod(K_curr_chunk_sobolev, axis=1)
+        start_idx = end_idx
     
-    gamma_matrix = approximate_k_set(lambda_matrix=lambda_matrix, lambda_X=lambda_X)
+    if verbose:
+        t1 = time_ns()
+        dt = (t1-t0) / 1e9
+        print(f'Wall-clock time of get_K_U_sobolev_vectorized with chunks of {chunk_size}: {dt:.3f} (s)')
 
-    return gamma_matrix
+    return K_sob_matrix
 
+def calculate_hsic_looped(K_U: ndarray, K_gamma: ndarray, verbose: bool = False) -> float:
+    if verbose:
+        t0 = time_ns()
+    n = K_U.shape[0]
+    HSIC_fellmann = 0.0
+    const_0 = 2.0 / (n * (n - 1))
+
+    for j in range(1, n):
+        for i in range(j):
+            HSIC_fellmann += (K_U[i, j] - 1.0) * K_gamma[i, j]
+    if verbose:
+        t1 = time_ns()
+        dt = (t1-t0) / 1e9
+        print(f'Wall-clock time of calculate_hsic_looped: {dt:.3f} (s)')
+    return const_0 * HSIC_fellmann
+
+def calculate_hsic_vectorized(K_U: ndarray, K_gamma: ndarray, verbose: bool = False) -> float:
+    if verbose:
+        t0 = time_ns()
+    n = K_U.shape[0]
+    const_0 = 2.0 / (n * (n - 1))
+
+    #upper-triangle indices (i < j)
+    up_tri_indices_i, up_tri_indices_j = np_triu_indices(n, k=1)
+
+    HSIC_fellmann = (K_U[up_tri_indices_i, up_tri_indices_j] - 1.0) * K_gamma[up_tri_indices_i, up_tri_indices_j]
+
+    if verbose:
+        t1 = time_ns()
+        dt = (t1-t0) / 1e9
+        print(f'Wall-clock time of calculate_hsic_vectorized: {dt:.3f} (s)')
+    return const_0 * np_sum(HSIC_fellmann)
+
+
+def _get_u_index_superset_one_hot_binstrs(dim_of_U, higher_order=False):
+    return [flipStr(i) for i in sorted(getIndexSuperset(dim_of_U, higher_order=higher_order), key=lambda x: directBinStrSum(x))]
+
+def hsic(data_directory: str,
+        mesh_directory: str = 'data/CDR/mesh_save_dir/rectangle.xdmf',
+        field_of_interest: str = 'temp_field',
+        n: int = 10, 
+        num_of_spatial_sampling_m: int = None,
+        test_domain: ndarray = np_array([[0,1],[0,0.5]]),
+        num_of_inputs: int = 5,
+        which_input_one_hot: str = None,
+        return_as_bool: bool = False,
+        g_constraint: float = 0,
+        shuffle_inputs: bool = False,
+        chunk_size=None):
+
+    input_data_dirs_list = get_data_file_dirs(data_directory, data_type='input_data')
+
+    n_max = len(input_data_dirs_list)
+    data_dir_indices = np_arange(0, n_max)
+    if shuffle_inputs:
+        np_shuffle(data_dir_indices)
+        
+    n_max = len(input_data_dirs_list)
+    if n < n_max:
+        data_dir_indices = data_dir_indices[:n]
+    else:
+        n = n_max
+    
+    input_data_dirs_list_to_use = [input_data_dirs_list[i] for i in data_dir_indices[:n]]
+    field_data_dirs_list_to_use = [dir.replace('input_data.npy', field_of_interest) for dir in input_data_dirs_list_to_use]
+
+    #load input data
+    u_arr = np_zeros((n, 5))
+    for i, dir in enumerate(field_data_dirs_list_to_use):
+        u_arr[i] = np_load(dir)
+
+    # need to convert input data to unif[0,1]!
+    if chunk_size is None or chunk_size <= 0:
+        chunk_size = int(n/5)
+    
+    inputs_one_hot_binstrs_list = _get_u_index_superset_one_hot_binstrs(dim_of_U=5)
+    if which_input_one_hot is None:
+        inputs_one_hot_binstrs_list_to_use = inputs_one_hot_binstrs_list
+    else:
+        if which_input_one_hot in inputs_one_hot_binstrs_list:
+            inputs_one_hot_binstrs_list_to_use = [which_input_one_hot, inputs_one_hot_binstrs_list_to_use[-1]]
+    K_U_dict = {}
+    for key in inputs_one_hot_binstrs_list_to_use:
+        K_U_dict[key] = get_K_U_sobolev_vectorized(input_data=u_arr,
+                                        n=n, 
+                                        num_of_inputs=5,
+                                        which_input_one_hot=key, 
+                                        chunk_size=chunk_size,
+                                        verbose=False)
+    for key in inputs_one_hot_binstrs_list_to_use[:-1]:
+        K_U_dict[key] = K_U_dict[key]/K_U_dict[-1]
+
+    
+    if num_of_spatial_sampling_m is None:
+        m = n
+    else:
+        m = num_of_spatial_sampling_m
+    my_mesh = load_mesh(mesh_dir=mesh_directory)
+    V = load_function_space(my_mesh)
+    K_gamma = get_K_gamma(field_data_dirs_list=field_data_dirs_list_to_use,
+                        n=n,
+                        num_of_spatial_sampling_m=m,
+                        mesh=my_mesh,
+                        func_space_V=V,
+                        test_domain=test_domain,
+                        return_as_bool=return_as_bool,
+                        g_constraint=g_constraint)
+    
+    hsic_vals_dict = {}
+    hsic_all = calculate_hsic_vectorized(K_U=K_U_dict[inputs_one_hot_binstrs_list_to_use[-1]], K_gamma=K_gamma, verbose=True)
+    for key in inputs_one_hot_binstrs_list_to_use[:-1]:
+        hsic_vals_dict[key] = calculate_hsic_vectorized(K_U=K_U_dict[key], K_gamma=K_gamma, verbose=True)/hsic_all
+    
+    return hsic_vals_dict
 """
 NOT!E: NEED TO TAKE THE INDICES OF DIRS OUT SINCE enteries of the outputs matrix k_gamma needs to 
 be able to be associated with the inputs matrix enteries K_u as each i,j. Also, port the port the vectorized
